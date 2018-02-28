3a3f75fc6e90d1440df21a262b7df43c8455fe21
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-02-28 11:20:33 -0500

    mobile responsiveness added to navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,37 +1,78 @@
-import React from "react"
+import React, { Component } from "react"
 import { NavLink } from "react-router-dom"
 
-const Navbar = props => (
-  <nav className={props.active}>
-    <h2>Jazzy.Codes</h2>
-    <div>
-      <NavLink to="/about" activeClassName="current">
-        <button>
-          <h3>About</h3>
-        </button>
-      </NavLink>
-      <NavLink to="/projects" activeClassName="current">
-        <button>
-          <h3>Projects</h3>
-        </button>
-      </NavLink>
-      <NavLink to="/presentations" activeClassName="current">
-        <button>
-          <h3>Presentations</h3>
-        </button>
-      </NavLink>
-      <NavLink to="/articles" activeClassName="current">
-        <button>
-          <h3>Articles</h3>
-        </button>
-      </NavLink>
-      <a href="/Jasmina_Jacquelina.pdf">
-        <button>
-          <h3>Resume</h3>
-        </button>
-      </a>
-    </div>
-  </nav>
-)
+class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuClick: ""
+    }
+    this.handleMenu = this.handleMenu.bind(this)
+    this.exitMenu = this.exitMenu.bind(this)
+  }
 
+  handleMenu() {
+    const menuClick = this.state.menuClick === "" ? "clicked" : ""
+    this.setState({ menuClick })
+  }
+
+  exitMenu() {
+    this.setState({ menuClick: "" })
+  }
+
+  render() {
+    const props = this.props
+    return (
+      <nav className={props.active}>
+        <h2>Jazzy.Codes</h2>
+        <div className={this.state.menuClick}>
+          <button id="menu-bars" onClick={this.handleMenu}>
+            <i className="fas fa-bars" />
+          </button>
+          <NavLink
+            to="/about"
+            activeClassName="current"
+            onClick={this.exitMenu}
+          >
+            <button className="navButton">
+              <h3>About</h3>
+            </button>
+          </NavLink>
+          <NavLink
+            to="/projects"
+            activeClassName="current"
+            onClick={this.exitMenu}
+          >
+            <button className="navButton">
+              <h3>Projects</h3>
+            </button>
+          </NavLink>
+          <NavLink
+            to="/presentations"
+            activeClassName="current"
+            onClick={this.exitMenu}
+          >
+            <button className="navButton">
+              <h3>Presentations</h3>
+            </button>
+          </NavLink>
+          <NavLink
+            to="/articles"
+            activeClassName="current"
+            onClick={this.exitMenu}
+          >
+            <button className="navButton">
+              <h3>Articles</h3>
+            </button>
+          </NavLink>
+          <a href="/Jasmina_Jacquelina.pdf">
+            <button className="navButton">
+              <h3>Resume</h3>
+            </button>
+          </a>
+        </div>
+      </nav>
+    )
+  }
+}
 export default Navbar
